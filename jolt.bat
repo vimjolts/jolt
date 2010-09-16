9cca938a7d47cfc98a35f0316ed2804f2266c73b
mattn <mattn.jp@gmail.com>
2010-09-16 09:28:31 +0900

    path safe.

--- jolt.bat
+++ jolt.bat
@@ -4,5 +4,10 @@ rem Windows Driver script for Jolt
 setlocal
 set JOLT=%~f0
 
-%~dp0..\python "%~dp0jolt" %*
+set PYTHON=%~dp0..\python.exe
+
+if not exist "%PYTHON%" set PYTHON=c:\python26\python.exe
+
+%PYTHON% "%~dp0jolt" %*
+
 endlocal
