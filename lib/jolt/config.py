09c12519654a45c355d86890d2884da1c665b0e6
tyru <tyru.exe@gmail.com>
2010-10-15 23:55:52 +0900

    Source config file.
    On Windows:
        path is %HOME%, %USERPROFILE%, and current directory (if exists)
        filename is _joltrc
    
    Other environment:
        path is $HOME
        filename is .joltrc

--- lib/jolt/config.py
+++ lib/jolt/config.py
@@ -11,6 +11,11 @@ def get_config():
     global config
     if not config:
         config = create_default_config()
+        config_file = look_up_config_file()
+        if config_file:
+            # NOTE: source_config_file() uses
+            # exec statement (which is likely eval()).
+            source_config_file(config_file, config)
     return config
 
 
@@ -23,3 +28,28 @@ def create_default_config():
     return {
         'vim_home_dir': vim_home_dir,
     }
+
+
+def look_up_config_file():
+    if sys.platform == 'win32':
+        path = []
+        for env in ['HOME', 'USERPROFILE']:
+            if os.environ.has_key(env):
+                path.append(os.environ[env])
+        path.append('.')
+        filename = '_joltrc'
+    else:
+        assert os.environ.has_key('HOME')
+        path = [os.environ['HOME']]
+        filename = '.joltrc'
+
+    for p in path:
+        f = os.path.join(p, filename)
+        if os.path.exists(f):
+            return f
+
+    return None
+
+def source_config_file(config_file, config):
+    with open(config_file) as f:
+        exec f.read()
